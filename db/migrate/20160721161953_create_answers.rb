220b6309fd15b19b6ee337b9cf7da524a956f037
Eric Booker & Georgia Rust & Kenneth Olson & nick bushell <pair+ericbooker12.grust101.kennetholson.nbushell4977@devbootcamp.com>
2016-07-22 15:38:54 -0700

    Write correct associations. Write seed file. :construction:

--- db/migrate/20160721161953_create_answers.rb
+++ db/migrate/20160721161953_create_answers.rb
@@ -3,6 +3,7 @@ class CreateAnswers < ActiveRecord::Migration
 		create_table :answers do |t|
 			t.string :content, :null => false
 			t.integer :user_id, :null => false
+			t.integer :question_id, :null => false
 
 			t.timestamps(null: false)
 		end
