33f7b93634fae157d691df1f0fc638da50194c4c
Eric Booker & nick bushell <pair+ericbooker12.nbushell4977@devbootcamp.com>
2016-07-22 16:43:08 -0700

    Add views

---
+++ app/controllers/question.rb
@@ -0,0 +1,24 @@
+# get '/questions' do
+
+# end
+
+# get '/questions/new' do
+#   erb :'/questions/new'
+# end
+
+# post '/questions' do
+#   @question = Question.new(params[:content])
+
+#   if @question.save
+#     redirect "/users/#{current_user.id}"
+#   else
+#     @error = "Error"
+#     erb :"/question/new"
+#   end
+# end
+
+# get '/questions/:id' do
+# end
+
+# delete '/questions/:id' do
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :answers
-  has_many :responses 
+  has_many :responses
   has_many :votes
 
+  has_secure_password
 
 
 
